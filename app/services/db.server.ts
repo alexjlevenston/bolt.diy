2966b24e9d2e8b2d7efffc5904078516deb3fef7
Alexander Levenston <alexlevenston2021@gmail.com>
2025-02-07 13:00:33 -0500

    use prisma accelerate

--- app/services/db.server.ts
+++ app/services/db.server.ts
@@ -1,18 +1,25 @@
 import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-let prisma: PrismaClient;
+// Define the type for the extended PrismaClient
+let prisma: ReturnType<typeof getPrismaClient>;
 
 declare global {
   // eslint-disable-next-line no-var
-  var __db: PrismaClient | undefined;
+  var __db: ReturnType<typeof getPrismaClient> | undefined;
+}
+
+// Helper function to create PrismaClient instance with Accelerate
+function getPrismaClient() {
+  return new PrismaClient().$extends(withAccelerate());
 }
 
 // This prevents multiple instances of Prisma Client in development
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
+  prisma = getPrismaClient();
 } else {
   if (!global.__db) {
-    global.__db = new PrismaClient();
+    global.__db = getPrismaClient();
   }
 
   prisma = global.__db;
